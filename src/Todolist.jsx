412bbb61d7ffc4ca0e8b862b15bfb7487b83bd74
Eric Mulaa <mutisyamulaa@gmail.com>
2024-02-21 15:30:54 +0300

    First commit

---
+++ src/Todolist.jsx
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+
+function ToDoList(){
+    const [tasks, setTasks] = useState([])
+    const [newTask, setNewTask] = useState('')
+
+    function change(e){
+        setNewTask(e.target.value)
+    }
+
+    function addTask(){
+        if(newTask.trim() !== ''){
+            setTasks(t => [...t, newTask])
+            setNewTask('')
+        }
+
+    }
+    
+    function deleteTask(index){
+        const updatedTasks = tasks.filter((_, i) => i !== index)
+        setTasks(updatedTasks)
+    }
+    
+    function moveUp(index) {
+        if (index > 0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
+    
+
+    
+    function moveDown(index){
+        if (index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
+
+
+    return(
+        <div>
+            <h1>To-do-List</h1>
+            <div className="input-button">
+                <input type="text" className="input" placeholder="Enter a new event" value={newTask} onChange={change} />
+                <button className="add-button" onClick={addTask}>Add</button>
+            </div>
+            <ol className="ordered-list">
+                {tasks.map((task, index) => <li className="list" key={index}>
+                    <span className="text">{task}</span>
+                    <button className="delete-button" onClick={() => deleteTask(index)}>❌</button>
+                    <button className="up-button" onClick={() => moveUp(index)}>⬆️</button>
+                    <button className="down-button" onClick={() => moveDown(index)}>⬇️</button>
+
+                </li>)}
+            </ol>
+    
+        </div>
+    )
+}
+
+export default ToDoList
