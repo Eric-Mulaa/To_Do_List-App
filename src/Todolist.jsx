468277be7e3c47498fc0a72c321f4c09745fd3b7
Eric Mulaa <mutisyamulaa@gmail.com>
2024-09-02 15:11:56 +0300

    commit

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -15,6 +15,11 @@ function ToDoList(){
         }
 
     }
+    function handleKeyPress(event){
+        if(event.key==='Enter'){
+            addTask()
+        }
+    }
     
     function deleteTask(index){
         const updatedTasks = tasks.filter((_, i) => i !== index)
@@ -42,13 +47,14 @@ function ToDoList(){
 
     return(
         <div>
-            <h1>To-do-List</h1>
+            <h1 className="heading">To-do-List</h1>
             <div className="input-button">
-                <input type="text" className="input" placeholder="Enter a new event" value={newTask} onChange={change} />
+                <input type="text" className="input" placeholder="Enter a new event" value={newTask} onChange={change} onKeyDown={handleKeyPress} />
                 <button className="add-button" onClick={addTask}>Add</button>
             </div>
             <ol className="ordered-list">
-                {tasks.map((task, index) => <li className="list" key={index}>
+                {tasks.map((task, index) => 
+                <li className="list" key={index}>
                     <span className="text">{task}</span>
                     <button className="delete-button" onClick={() => deleteTask(index)}>❌</button>
                     <button className="up-button" onClick={() => moveUp(index)}>⬆️</button>
